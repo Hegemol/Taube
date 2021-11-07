ac433e1a0a0a7d74ab3aee8e453409e596a553d6
KevinClair <704714211@qq.com>
2021-11-08 01:20:34 +0800

    常量类。

---
+++ taube-common/src/main/java/org/hegemol/taube/common/constants/CommonConstants.java
@@ -0,0 +1,13 @@
+package org.hegemol.taube.common.constants;
+
+/**
+ * 常量池.
+ *
+ * @author KevinClair
+ **/
+public class CommonConstants {
+    /**
+     * 读写超时时间
+     */
+    public static final Integer TIMEOUT_SECONDS = 30;
+}
