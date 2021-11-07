e6e9ccfdfb7ffac4593753c75810d5a206c0d3b8
KevinClair <704714211@qq.com>
2021-11-07 20:28:10 +0800

    消费接口；

---
+++ taube-client/src/main/java/org/hegemol/taube/client/consumer/handle/TaubeConsumer.java
@@ -0,0 +1,20 @@
+package org.hegemol.taube.client.consumer.handle;
+
+import org.hegemol.taube.client.common.model.MessageResult;
+
+/**
+ * 消息监听，消息消费实现此接口即可；
+ *
+ * @author KevinClair
+ **/
+public interface TaubeConsumer {
+
+    /**
+     * 消息消费。
+     *
+     * @param data 消息内容
+     * @return 消费状态 {@link MessageResult}
+     * @throws Exception
+     */
+    MessageResult message(Object data) throws Exception;
+}
