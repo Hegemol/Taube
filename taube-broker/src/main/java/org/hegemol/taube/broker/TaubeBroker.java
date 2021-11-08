7909de25276892f715e5b60ae9bab3ecc043daac
KevinClair <704714211@qq.com>
2021-11-09 00:03:42 +0800

    Broker启动服务端。

--- taube-broker/src/main/java/org/hegemol/taube/broker/TaubeBroker.java
+++ taube-broker/src/main/java/org/hegemol/taube/broker/TaubeBroker.java
@@ -1,11 +1,9 @@
 package org.hegemol.taube.broker;
 
-import org.hegemol.taube.broker.config.TaubeBrokerConfigurationProperty;
-import org.hegemol.taube.common.utils.IpUtils;
+import org.hegemol.taube.broker.netty.NettyServer;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
-import java.net.UnknownHostException;
 
 /**
  * broker客户端;
@@ -15,25 +13,16 @@ import java.net.UnknownHostException;
 @Component
 public class TaubeBroker implements InitializingBean, DisposableBean {
 
-    private TaubeBrokerConfigurationProperty brokerConfigurationProperty;
-
-    public TaubeBroker(final TaubeBrokerConfigurationProperty brokerConfigurationProperty) {
-        this.brokerConfigurationProperty = brokerConfigurationProperty;
-    }
+    private NettyServer nettyServer;
 
     @Override
     public void destroy() throws Exception {
-        // 初始化服务端
-        initServer();
-    }
 
-    private void initServer() throws UnknownHostException {
-        final String ip = IpUtils.getIp();
-        String address = String.join(":", ip, brokerConfigurationProperty.getPort().toString());
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
-
+        // 初始化服务端
+        nettyServer.start();
     }
 }
