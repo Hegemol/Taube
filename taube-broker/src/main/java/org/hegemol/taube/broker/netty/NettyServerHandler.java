b5941278936e5c0785bb13bbd3e0dbe1d9ab3970
KevinClair <704714211@qq.com>
2021-11-09 00:07:11 +0800

    Netty服务端处理器.

--- taube-broker/src/main/java/org/hegemol/taube/broker/netty/NettyServerHandler.java
+++ taube-broker/src/main/java/org/hegemol/taube/broker/netty/NettyServerHandler.java
@@ -1,17 +1,22 @@
 package org.hegemol.taube.broker.netty;
 
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import org.springframework.stereotype.Component;
 
 /**
  * Netty服务端处理器.
  *
  * @author KevinClair
  **/
+@ChannelHandler.Sharable
+@Component
 public class NettyServerHandler extends SimpleChannelInboundHandler<Object> {
 
     @Override
     protected void channelRead0(final ChannelHandlerContext channelHandlerContext, final Object o) throws Exception {
 
+        // 通过Tcp header中的类型来判断当前的请求是发送消息请求还是拉取消息请求
     }
 }
