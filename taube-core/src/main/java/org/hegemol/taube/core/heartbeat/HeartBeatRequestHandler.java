04d441970e947a0e51ae8032622f740a4d6ffaa0
KevinClair <704714211@qq.com>
2021-11-13 16:04:57 +0800

    心跳处理

--- taube-core/src/main/java/org/hegemol/taube/core/heartbeat/HeartBeatRequestHandler.java
+++ taube-core/src/main/java/org/hegemol/taube/core/heartbeat/HeartBeatRequestHandler.java
@@ -5,6 +5,8 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.ReferenceCountUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.hegemol.taube.common.enums.MessageTypeEnum;
+import org.hegemol.taube.common.model.MessageModel;
 
 /**
  * 心跳请求处理器
@@ -12,11 +14,31 @@ import lombok.extern.slf4j.Slf4j;
  * @author KevinClair
  **/
 @ChannelHandler.Sharable
+@Slf4j
 public class HeartBeatRequestHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        // TODO 心跳请求
+        final MessageModel requestModel = (MessageModel) msg;
+        // CONSUMER心跳请求
+        if (requestModel.getType().equals(MessageTypeEnum.CONSUMER_HEART_BEAT_RQEUEST)){
+            MessageModel responseModel = new MessageModel();
+            responseModel.setType(MessageTypeEnum.CONSUMER_HEART_BEAT_RESPONSE);
+            ctx.channel().writeAndFlush(responseModel);
+            ReferenceCountUtil.release(msg);
+            return;
+        }
 
+        // PRODUCER心跳请求
+        if (requestModel.getType().equals(MessageTypeEnum.PRODUCER_HEART_BEAT_RQEUEST)){
+            MessageModel responseModel = new MessageModel();
+            responseModel.setType(MessageTypeEnum.PRODUCER_HEART_BEAT_RESPONSE);
+            ctx.channel().writeAndFlush(responseModel);
+            ReferenceCountUtil.release(msg);
+            return;
+        }
+
+        // 继续后续请求
+        ctx.fireChannelRead(msg);
     }
 }
--- taube-core/src/main/java/org/hegemol/taube/core/heartbeat/HeartBeatResponseHandler.java
+++ taube-core/src/main/java/org/hegemol/taube/core/heartbeat/HeartBeatResponseHandler.java
@@ -3,6 +3,9 @@ package org.hegemol.taube.core.heartbeat;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+import org.hegemol.taube.common.enums.MessageTypeEnum;
+import org.hegemol.taube.common.model.MessageModel;
 
 /**
  * 心跳响应处理器
@@ -15,5 +18,20 @@ public class HeartBeatResponseHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         // TODO 心跳回应
+        MessageModel responseModel = (MessageModel) msg;
+        // CONSUMER心跳响应
+        if (responseModel.getType().equals(MessageTypeEnum.CONSUMER_HEART_BEAT_RQEUEST)){
+            ReferenceCountUtil.release(msg);
+            return;
+        }
+
+        // PRODUCER心跳响应
+        if (responseModel.getType().equals(MessageTypeEnum.PRODUCER_HEART_BEAT_RQEUEST)){
+            ReferenceCountUtil.release(msg);
+            return;
+        }
+
+        // 继续后续请求
+        ctx.fireChannelRead(msg);
     }
 }
