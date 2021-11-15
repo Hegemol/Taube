6c06733de07c4bbbcd4dec0c7fd489505c658a8b
KevinClair <704714211@qq.com>
2021-11-15 15:49:27 +0800

    Add topic service.

---
+++ taube-core/src/main/java/org/hegemol/taube/core/service/TopicService.java
@@ -0,0 +1,16 @@
+package org.hegemol.taube.core.service;
+
+/**
+ * Topic处理
+ *
+ * @author KevinClair
+ **/
+public interface TopicService {
+
+    /**
+     * 创建topic
+     *
+     * @param name topic姓名
+     */
+    void createTopic(String name);
+}
---
+++ taube-core/src/main/java/org/hegemol/taube/core/service/impl/TopicServiceImpl.java
@@ -0,0 +1,18 @@
+package org.hegemol.taube.core.service.impl;
+
+import org.hegemol.taube.core.service.TopicService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Topic服务实现
+ *
+ * @author KevinClair
+ **/
+@Service
+public class TopicServiceImpl implements TopicService {
+
+    @Override
+    public void createTopic(final String name) {
+
+    }
+}
