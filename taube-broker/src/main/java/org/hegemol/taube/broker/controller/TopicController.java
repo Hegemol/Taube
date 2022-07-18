0034b5c272764f3d914d8c710977dbdc7f2e4313
KevinClair <704714211@qq.com>
2022-07-18 23:37:52 +0800

    调整controller的注入属性

--- taube-broker/src/main/java/org/hegemol/taube/broker/controller/TopicController.java
+++ taube-broker/src/main/java/org/hegemol/taube/broker/controller/TopicController.java
@@ -1,8 +1,12 @@
 package org.hegemol.taube.broker.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.hegemol.taube.broker.model.Result;
+import org.hegemol.taube.broker.model.vo.TopicCreateRequest;
 import org.hegemol.taube.broker.mysql.TopicMapper;
 import org.hegemol.taube.broker.mysql.model.Topic;
+import org.hegemol.taube.broker.service.TopicService;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,12 +19,30 @@ import java.util.List;
  **/
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/topic")
 public class TopicController {
 
-    private final TopicMapper topicMapper;
+    private final TopicService topicService;
 
+    /**
+     * 查询所有的topic
+     *
+     * @return 所有的topic列表
+     */
     @RequestMapping("/select")
     public List<Topic> select(){
-        return topicMapper.select();
+        return topicService.select();
+    }
+
+    /**
+     * 创建topic
+     *
+     * @param request 请求参数 {@link TopicCreateRequest}
+     * @return 创建是否成功
+     */
+    @RequestMapping("/create")
+    public Result<String> create(@RequestBody TopicCreateRequest request){
+        topicService.createTopic(request);
+        return Result.success("success");
     }
 }
